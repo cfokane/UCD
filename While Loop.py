71ceeb523bebecdce44c47a73a51f52e8dc03e90
cfokane <colmfokane@gmail.com>
2021-01-03 18:18:16 +0000

    fourth committ

--- While Loop.py
+++ While Loop.py
@@ -14,3 +14,12 @@ while offset != 0:
         print("correcting...")
         offset = offset - 1
         print(offset)
+
+offset=16
+while offset != 0 :
+    print("correcting...")
+    if (offset>0) :
+      offset=offset-1
+    else :
+      offset=offset+1
+    print(offset)
