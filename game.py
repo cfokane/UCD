c99c05a42a3814870917c1ee490b9a57a0758c04
cfokane <colmfokane@gmail.com>
2021-01-06 11:01:48 +0000

    first commit

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 np.random.seed(123)
-coin=np.random.randint(0, 2)
+coin = np.random.randint(0, 2)
 print(coin)
-if coin == 0 :
+if coin == 0:
     print('heads')
-else :
-    print ('tails')
+else:
+    print('tails')
+
+np.random.seed(123)
+tails=[0]
+for x in range[10] :
+    coin=np.random.randint(0, 2)
+    tails.append(tails[x] + coin)
