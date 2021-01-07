0921f33418030510912efa5967a65bfd48069cb0
cfokane <colmfokane@gmail.com>
2021-01-07 20:09:28 +0000

    second commit

--- DataCampWork/transformed_data_enhanced.py
+++ DataCampWork/transformed_data_enhanced.py
@@ -3,10 +3,13 @@ df = pd.read_csv('transformed_data.csv', index_col=0)
 for val in df :
     print(val)
 
-df['DATE'] = pd.to_datetime(df['DATE'],format='%Y/%m/%d')
+df['DATE'] = pd.to_datetime(df['DATE'], format='%Y/%m/%d')
 df['Week_Num'] = df['DATE'].dt.strftime('%U')
 df['day'] = df['DATE'].dt.strftime('%A')
-df['date'] = pd.to_datetime(df['DATE'],format='%d/%m/%Y')
+df['date'] = pd.to_datetime(df['DATE'], format='%d/%m/%Y')
+print(df.head(10))
+
+df['dates'] = pd.to_datetime(df['DATE'], format='%Y %m %d')
 print(df.head(10))
 
 
