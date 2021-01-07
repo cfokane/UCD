6516f805bb61e7e8e2560b8d75196b837e2c0911
cfokane <colmfokane@gmail.com>
2021-01-07 17:42:51 +0000

    first commit

--- DataCampWork/WorldDate_to_Calendar.py
+++ DataCampWork/WorldDate_to_Calendar.py
@@ -3,14 +3,13 @@ date_period = pd.read_csv('countries_of_the_world.csv', index_col=0)
 # explore column names
 for val in date_period:
     print(val)
+print(date_period.columns)
 print(date_period[0:1])
 print(date_period)
 print(date_period.tail)
-print(date_period.columns)
 print(date_period[1:4])
 print(date_period.loc[['Albania ', 'Zambia '], ['Region']])
 print(date_period.loc[:, ['Region']])
 print(date_period.loc[:, :])
 sel=[date_period['Region']]
 print(sel)
-
