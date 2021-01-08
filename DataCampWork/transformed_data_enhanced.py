5081ddad925d88a93244fff780f0a60d9e92d2e4
cfokane <colmfokane@gmail.com>
2021-01-08 13:12:53 +0000

    second commit

--- DataCampWork/transformed_data_enhanced.py
+++ DataCampWork/transformed_data_enhanced.py
@@ -1,15 +1,34 @@
-import pandas as pd
-df = pd.read_csv('transformed_data.csv', index_col=0)
-for val in df :
-    print(val)
+from typing import Any, Union
 
+import pandas as pd
+from pandas import Series, DataFrame
+from pandas.io.parsers import TextFileReader
+
+df = pd.read_csv('transformed_data.csv')
 df['DATE'] = pd.to_datetime(df['DATE'], format='%Y/%m/%d')
+# Create Keys
 df['Week_Num'] = df['DATE'].dt.strftime('%U')
 df['day'] = df['DATE'].dt.strftime('%A')
-df['date'] = pd.to_datetime(df['DATE'], format='%d/%m/%Y')
+df['dayz'] = df['DATE'].dt.strftime('%j')
+df['Dates'] = df['DATE'].dt.strftime('%Y%m%d')
+# concatenate columns for Keys from Country Code Week and Day numbers
+df['Wkdayz'] = df['Week_Num'] + df['dayz']
+df['Con_cat'] = df['CODE'] + df['Wkdayz']
+df['Cases per M Pop'] = df['TC'] / df['POP']
+for val in df:
+    print(val)
+print(df)
 print(df.head(10))
 
-df['dates'] = pd.to_datetime(df['DATE'], format='%Y %m %d')
-print(df.head(10))
+#for lab, row in df.iterrows() :
+#   print(lab)
+#   print(row)
+#df = pd.read_csv('transformed_data.csv', index_col=0)
+#for lab, row in df.iterrows() :
+#     print(lab + ": " + str(row['COUNTRY']))
 
-
+#df= pd.read_csv('transformed_data.csv', index_col = 0)
+#for lab, row in df.iterrows() :
+#    df.loc[lab, 'COUNTRYS'] = row['index_col=0'].upper()
+#    df.loc[lab,'key_'] = df['CODE'] + df['Wkdayz']
+#    print(df)
