a259d2f37ade6e30c3624ee3cfbca8c063fc5186
cfokane <colmfokane@gmail.com>
2021-01-14 13:02:14 +0000

    14/01 commit2

--- DataCampWork/stingency_enhanced.py
+++ DataCampWork/stingency_enhanced.py
@@ -1,13 +1,18 @@
 import pandas as pd
-df = pd.read_csv('covid-stringency-index.csv', index_col=0)
-print(df)
+S_E = pd.read_csv('covid-stringency-index.csv', index_col=0)
+E_U = pd.read_csv('states.csv', index_col=0)
+#print(S_E.head(5))
+#print(E_U.head(10))
+#for val in S_E :
+#    print(val)
+#print(S_E.head(3))
+#print(S_E.info())
+#print(S_E.describe())
 
-for val in df :
-    print(val)
-print(df.head(3))
-print(df.info())
-print(df.describe())
-
-#print(df.shape)
-print(df.columns)
-#print(df.index)
+#print(S_E.shape)
+#print(S_E.columns)
+#print(E_U.columns)
+#print(S_E.index)
+#print(E_U.index)
+EU=(pd.concat([S_E, E_U], sort=True))
+print(EU.head)
