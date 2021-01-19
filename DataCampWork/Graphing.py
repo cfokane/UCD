fd4ca0a489d59ef325721383cc9bcb2815e62356
cfokane <colmfokane@gmail.com>
2021-01-19 16:08:52 +0000

    19/01 commit1

--- DataCampWork/Graphing.py
+++ DataCampWork/Graphing.py
@@ -6,7 +6,7 @@ print(EU.head)
 import matplotlib.pyplot as plt
 import seaborn as sns
 # Create a scatter plot of absences vs. final grade
-sns.scatterplot(x='Week_Num', y='Weekly_Cases', data=EU, size='Weekly_Cases', hue='Weekly_Cases', alpha=0.4)
+sns.scatterplot(x='Week_Num', y='total_cases', data=EU, size='total_cases', hue='total_cases', alpha=0.4)
 #time, cases or deaths and stringency indexed 1-5
 # Show plot
 plt.show()
