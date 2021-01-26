c58984496ef2d9195146cfe7302dc9b2f3c083be
cfokane <colmfokane@gmail.com>
2021-01-26 08:45:28 +0000

    24/01 commit - renamed file

--- DataCampWork/Graph_of_spread using RDCD8.py
+++ DataCampWork/Graph_of_spread using RDCD8.py
@@ -8,18 +8,18 @@ data = pd.read_csv('RDCD8.csv', index_col=0)
 print(data)
 from matplotlib.pyplot import rcParams
 fig, ax =plt.subplots()
-rcParams['figure.figsize']=10,6 # to change size of graph
-plt.plot(data.Grand_Total, color='m', marker='o', linestyle='-', label='Cases Recorded')
+#rcParams['figure.figsize']=15,2 # to change size of graph
+plt.plot(data.Grand_Total_Cases, color='m', marker='o', linestyle='-', label='Cases Recorded')
 #plt.plot(data.Week_Num)
 plt.grid(True, color='g',linestyle=':')
 plt.xlabel('Week Number')
-plt.ylim(0, 60000) #to set the axis ...xlim for x axis
-plt.yticks((0, 10000, 20000, 30000, 40000, 50000, 60000))
+plt.ylim(0, 2500000) #to set the axis ...xlim for x axis
+plt.yticks((0, 500000, 1000000, 1500000, 2000000, 2500000, 3000000))
 plt.ylabel('Number of Countries with Confirmed Cases')
-plt.title('Speed of global spread COVID: Cases recorded Q1-Q3 2020')
+plt.title('Fig 8; Growth in COVID Cases: Cases recorded Q1-Q3 2020')
 plt.legend(loc=2)
-ax.annotate('Wk15 = Wave1 Peak', xy=(16, 52000),  xycoords='data',
-                xytext=(15, 10), textcoords='offset points',
+ax.annotate('Wk15 = Wave1 Peak', xy=(15, 520000),  xycoords='data',
+                xytext=(15, 100), textcoords='offset points',
                 arrowprops=dict(arrowstyle="->")
                 )
 #ax.legend()
