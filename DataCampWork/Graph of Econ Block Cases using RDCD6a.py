2f9706e3ee4b1408fb21073bf6151c1517537ee2
cfokane <colmfokane@gmail.com>
2021-01-29 14:00:14 +0000

    29/01 commit

--- DataCampWork/Graph of Econ Block Cases using RDCD6a.py
+++ DataCampWork/Graph of Econ Block Cases using RDCD6a.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-white')
 
 # Plot GDP/Year
-data = pd.read_csv('RDCD6a.csv', index_col=0)
+data = pd.read_csv('RDCDFinal.csv', index_col=0)
 print(data)
 fig, ax = plt.subplots()
 #seventies = climate_change["1970-01-01":"1979-12-31"]
@@ -17,12 +17,12 @@ ax.plot(Q1_Q3.index, Q1_Q3['US'], label='US')
 ax.plot(Q1_Q3.index, Q1_Q3['All_Others'], label='All Others')
 #plt.xlim(1, 39) #to set the axis ...xlim for x axis
 #plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
-plt.ylim(0, 4500000) #to set the axis ...xlim for x axis
-plt.yticks([500000, 1000000, 1500000, 2000000, 2500000, 3000000, 3500000, 4000000, 4500000],['0.5m', '1.0m', '1.5m', '2.0m', '2.5m', '3.0m', '3.5m', '4.0m', '4.5m'])
+plt.ylim(0, 50000) #to set the axis ...xlim for x axis
+plt.yticks([5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000],['0.5k', '1.0k', '1.5k', '2.0k', '2.5k', '3.0k', '3.5k', '4.0k', '4.5k'])
 ax.set_xlabel('Q1-Q3 2020 (Months)')
-ax.set_ylabel("COVID Cases Reported (Millions)")
-ax.set_title("COVID Cases by Month in select Economic Blocks")
+ax.set_ylabel("COVID Deaths Reported (Millions)")
+ax.set_title("COVID Deaths by Month in select Economic Blocks")
 ax.legend(loc=2)
 plt.show()
-fig.savefig('COVID Cases by Month in select Economic Blocks.png')
+fig.savefig('COVID Deaths by Month in select Economic Blocks.png')
 plt.close()
